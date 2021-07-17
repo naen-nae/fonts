d4a4e43d47928adef88388e96bea133700c4aafc
shj <kan02134@gmail.com>
2021-07-18 04:37:06 +0900

    fix: 모든 subset fonts 파일이 만들어 질 때 까지 대기

--- scripts/fontGen/genSubsetFontFiles.js
+++ scripts/fontGen/genSubsetFontFiles.js
@@ -9,35 +9,41 @@ module.exports = async fontsObj => {
   // gen subset font files
   await genNewDir('subset-fonts');
 
-  _.map(fontsObj, async ({ files: filePaths }, ind, { length }) => {
-    // gen subset dir
-    const dir = filePaths[0].split('/')[0];
-    await genNewDir(`subset-fonts/${dir}`);
+  await Promise.all(
+    _.partial(_.map, _, async ({ files: filePaths }) => {
+      // gen subset dir
+      const dir = filePaths[0].split('/')[0];
+      await genNewDir(`subset-fonts/${dir}`);
 
-    await _.flow(
-      // get font bufs
-      _.partial(_.map, _, async filePath => ({
-        path: filePath,
-        buf: await fs.readFile(`./files/${filePath}`),
-      })),
-      // gen subset bufs
-      _.partial(_.map, _, async file => {
-        const { path, buf } = await file;
+      console.log(`generating subset font files for ${dir}`);
 
-        return {
-          path,
-          buf: await subsetFont(buf, '다람쥐 헌 쳇바퀴에 타고파', {
-            targetFont: 'woff2',
+      return await Promise.all(
+        _.flow(
+          // get font bufs
+          _.partial(_.map, _, async filePath => ({
+            path: filePath,
+            buf: await fs.readFile(`./files/${filePath}`),
+          })),
+          // gen subset bufs
+          _.partial(_.map, _, async file => {
+            const { path, buf } = await file;
+
+            return {
+              path,
+              buf: await subsetFont(buf, '다람쥐 헌 쳇바퀴에 타고파', {
+                targetFont: 'woff2',
+              }),
+            };
           }),
-        };
-      }),
-      // gen subset font file
-      _.partial(_.forEach, _, async subset => {
-        const { path, buf } = await subset;
-        return fs.writeFile(`./build/subset-fonts/${path}.woff2`, buf);
-      }),
-    )(filePaths);
+          // gen subset font file
+          _.partial(_.map, _, async subset => {
+            const { path, buf } = await subset;
+            return fs.writeFile(`./build/subset-fonts/${path}.woff2`, buf);
+          }),
+        )(filePaths),
+      );
+    })(fontsObj),
+  );
 
-    console.log(`>> start gen '`, dir, `' fonts (${ind + 1} / ${length})`);
-  });
+  console.log(`>> subset-fonts done`);
 };
