0c41e994be1f3e90fb9d0fb9df4f57ff88f38ad6
shj <kan02134@gmail.com>
2021-09-28 13:54:59 +0900

    fix: remove inappropriate font
    - KoreanFrenchTypewriter (https://git.io/JzFJD)

--- scripts/buildFiles.js
+++ scripts/buildFiles.js
@@ -3,7 +3,10 @@ const git = simpleGit();
 const fontGen = require('./fontGen');
 
 (async () => {
-  const { files } = await git.status();
+  const status = await git.status();
+  const { files } = status;
+
+  console.log(status);
 
   if (files.some(({ path }) => path.startsWith('files/'))) {
     await fontGen();
