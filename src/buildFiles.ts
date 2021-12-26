c26499f02f90066d8a6e7615b70b8261b358d217
Gumball12 <to@shj.rip>
2021-12-26 23:59:42 +0900

    feat: remove unnecessary diff check logic

--- src/buildFiles.ts
+++ src/buildFiles.ts
@@ -1,17 +1,2 @@
-import simpleGit from 'simple-git';
 import fontGen from './fontGen';
-
-const git = simpleGit();
-
-(async () => {
-  const diff = await git.diff(['fonts.yml']);
-  const isDiff =
-    diff
-      .split('\n')
-      .filter(line => line.startsWith('- ') || line.startsWith('+ ')).length >
-    0;
-
-  if (isDiff) {
-    await fontGen();
-  }
-})();
+fontGen();
