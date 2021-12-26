c748a8942aef93b556d9177557737acf62b750b5
Gumball12 <to@shj.rip>
2021-12-27 00:02:44 +0900

    chore(git): ignore built files

--- built/fontGen/utils/toCdnSrc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * to jsdelivr cdn src
- *
- * @param src origin src
- * @param branch repo branch name
- */
-exports.default = (src, branch) => `https://cdn.jsdelivr.net/gh/naen-nae/fonts${branch !== undefined ? `@${branch}` : ''}/${src.replace(/^\//, '')}`;
